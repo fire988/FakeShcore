d412ff98bbb1566cd727a6dd115abfb8263266a2
jamie <98386@qq.com>
2021-12-22 15:03:53 +0800

    first commit

---
+++ FakeShcore/FakeShcore.cpp
@@ -0,0 +1,14 @@
+﻿// NetDetect.cpp : 定义 DLL 的导出函数。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "FakeShcore.h"
+#include <WinInet.h>
+
+FakeShcore_API HRESULT GetDpiForMonitor(HMONITOR hmonitor,int dpiType,UINT* dpiX,UINT* dpiY)
+{
+    *dpiX = 96;
+    *dpiY = 96;
+    return S_OK;
+}
